0e236a7e2297e1455a51fda2e0f2954f0dc12b24
Lucas Dogo <lucasdogo01@gmail.com>
2025-07-31 17:15:47 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasdogo/SpringBootMongo/services/UserService.java
+++ src/main/java/com/lucasdogo/SpringBootMongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasdogo.SpringBootMongo.services;
 
 import com.lucasdogo.SpringBootMongo.domain.User;
+import com.lucasdogo.SpringBootMongo.dto.UserDTO;
 import com.lucasdogo.SpringBootMongo.repository.UserRepository;
 import com.lucasdogo.SpringBootMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
