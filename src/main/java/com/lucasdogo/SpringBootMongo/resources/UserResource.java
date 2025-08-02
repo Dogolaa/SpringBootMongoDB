bc211c1f67c43c6e288fdc75a67ca22316cb6c49
Lucas Dogo <lucasdogo01@gmail.com>
2025-08-01 23:07:01 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/lucasdogo/SpringBootMongo/resources/UserResource.java
+++ src/main/java/com/lucasdogo/SpringBootMongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lucasdogo.SpringBootMongo.resources;
 
+import com.lucasdogo.SpringBootMongo.domain.Post;
 import com.lucasdogo.SpringBootMongo.domain.User;
 import com.lucasdogo.SpringBootMongo.dto.UserDTO;
 import com.lucasdogo.SpringBootMongo.services.UserService;
@@ -52,6 +53,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
