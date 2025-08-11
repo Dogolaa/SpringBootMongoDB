7754d703ae271d0765df8a377327036d30813cfb
Lucas Dogo <lucasdogo01@gmail.com>
2025-08-11 18:06:40 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasdogo/SpringBootMongo/services/PostService.java
+++ src/main/java/com/lucasdogo/SpringBootMongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.lucasdogo.SpringBootMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,9 +14,13 @@ public class PostService {
 
     @Autowired
     private PostRepository repo;
-    
+
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
